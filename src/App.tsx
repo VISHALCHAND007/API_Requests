c0d868a21bf67f39c0dc6baec7ad3af67eb68b58
Vishal Chand <sam.chand96@gmail.com>
2024-12-11 17:33:16 +0530

    Integrated the list of comments with get request.

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ import {
 } from 'react-native';
 
 function App(): React.JSX.Element {
-  const [data, setData] = useState(undefined);
+  const [data, setData] = useState([]);
   const getComment = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    const url = 'https://jsonplaceholder.typicode.com/posts';
     const response = await fetch(url);
     let jsonResponse = await response.json();
 
@@ -26,30 +26,52 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar />
-      <Text style={styles.sectionTitle}>Comment</Text>
-      {data ? (
-        <View>
-          <Text style={[styles.textWhite, styles.underline]} >
-            User Id:{' '}
-            <Text style={{fontWeight: 'bold', fontSize: 15, textDecorationLine: 'none'}}>
-              {data['userId']}
-            </Text>
-          </Text>
-          <Text style={[styles.textWhite, styles.underline]}>
-            Title:{' '}
-            <Text style={{fontWeight: 'bold', fontSize: 15, textDecorationLine: 'none'}}>
-              {data['title']}
-            </Text>
-          </Text>
-          <Text style={[styles.textWhite, styles.underline]}>
-            Comment:{' '}
-            <Text style={{fontWeight: 'bold', fontSize: 15, textDecorationLine: 'none'}}>
-              {data['body']}
-            </Text>
-          </Text>
-        </View>
-      ) : null}
+      <ScrollView>
+      <Text style={styles.sectionTitle}>List of Comments</Text>
+      {data.length
+        ? data.map((item) => (
+            <View key={item['id']} style={styles.commentContainer}>
+              <Text
+                style={[
+                  styles.underline,
+                  {backgroundColor: '#ddd', marginBottom: 5},
+                ]}>
+                Id:{' '}
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    textDecorationLine: 'none',
+                  }}>
+                  {item['id']}
+                </Text>
+              </Text>
+              <Text style={styles.underline}>
+                Title:{' '}
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    textDecorationLine: 'none',
+                  }}>
+                  {item['title']}
+                </Text>
+              </Text>
+              <Text style={styles.underline}>
+                Comment:{' '}
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    textDecorationLine: 'none',
+                  }}>
+                  {item['body']}
+                </Text>
+              </Text>
+            </View>
+          ))
+        : null}
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -58,17 +80,22 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 32,
     paddingHorizontal: 24,
+    backgroundColor: '#ffffff',
+  },
+  commentContainer: {
+    padding: 10,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: '#ffffff',
   },
   textWhite: {
     color: '#ffffff',
   },
   underline: {
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
   },
   sectionDescription: {
     marginTop: 8,
