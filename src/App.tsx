68e9d4198743ed29afb03a5cea11cd173e414b73
Vishal Chand <sam.chand96@gmail.com>
2024-12-12 13:19:27 +0530

    Set the app to get the values from the input fields and then add the user to the list and show the added user in the below flat list.

--- src/App.tsx
+++ src/App.tsx
@@ -3,64 +3,109 @@ import Snackbar from 'react-native-snackbar';
 import {
   Button,
   FlatList,
+  KeyboardAvoidingView,
   SafeAreaView,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
 
-const localHost = 'http://192.168.1.17:3000'
+const localHost = 'http://192.168.1.17:3000';
 
 function App(): React.JSX.Element {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
   const [data, setData] = useState([]);
-  const getUsers = async () => {
+
+  const getUserData = async () => {
     const url = `${localHost}/users`;
-    const response = await fetch(url);
-    let jsonResponse = await response.json();
+    let response = await fetch(url);
+    const jsonResp = await response.json();
 
-    setData(jsonResponse);
-  };
-
-  const postUserData = async () => {
-      const user = {
-        name: 'Dheeraj Lohani', 
-        age: 24, 
-        email: 'dheeraj@gmail.com'
-      }
-      const url = `${localHost}/users`
-      let response = await fetch(url, {
-        method: 'POST', 
-        body: JSON.stringify(user), 
-        headers: {'Content-Type': 'application/json'}
-      })
-      response = await response.json()
-      Snackbar.show({
-        duration: Snackbar.LENGTH_LONG, 
-        text: 'Added successfully'
-      })
-      getUsers()
+    setData(jsonResp);
   };
 
   useEffect(() => {
-    getUsers();
+    getUserData();
   }, []);
+
+  const postUserData = async () => {
+    if (name.length && age.length && email.length) {
+      setError('');
+      const user = {
+        name: name,
+        age: age,
+        email: email,
+      };
+      const url = `${localHost}/users`;
+      let response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(user),
+      });
+      response = await response.json();
+      Snackbar.show({
+        textColor: '#ffffff',
+        duration: Snackbar.LENGTH_LONG,
+        text: 'Added successfully',
+      });
+      getUserData();
+    } else {
+      setError('All fields are required.');
+      Snackbar.show({
+        textColor: '#ffffff',
+        text: error,
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.sectionTitle}>FlatList: Get Users</Text>
+      <KeyboardAvoidingView>
+        <Text style={styles.sectionTitle}>FlatList: Post Users</Text>
+
+        <View>
+          <TextInput
+            placeholder="Enter name"
+            placeholderTextColor="grey"
+            value={name}
+            onChangeText={text => setName(text)}
+            style={styles.inputText}
+          />
+          <TextInput
+            placeholder="Enter age"
+            placeholderTextColor="grey"
+            value={age.toString()}
+            onChangeText={text => setAge(text)}
+            style={styles.inputText}
+          />
+          <TextInput
+            placeholder="Enter email"
+            placeholderTextColor="grey"
+            value={email}
+            onChangeText={text => setEmail(text)}
+            style={[styles.inputText, {marginBottom: 25}]}
+          />
+        </View>
+        <Button title="Add user" onPress={postUserData} />
+      </KeyboardAvoidingView>
+
       {data ? (
         <FlatList
-          data={data}
+          data={[...data].reverse()}
           renderItem={({item}) => (
-            <View style={styles.commentContainer}>
-              <Text style={styles.idText}>{item['name']}</Text>
-              <Text>Age: {item['age']}</Text>
-              <Text>Email: {item['email']}</Text>
+            <View style={styles.viewContainer}>
+              <Text style={{backgroundColor: 'orange'}}>{item['name']}</Text>
+              <Text>{item['age']}</Text>
+              <Text>{item['email']}</Text>
             </View>
           )}
         />
       ) : null}
-      <Button title="Testing post" onPress={postUserData} />
     </SafeAreaView>
   );
 }
@@ -72,36 +117,24 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     backgroundColor: '#ffffff',
   },
-  commentContainer: {
-    padding: 10,
-    borderBottomColor: 'orange',
-    borderBottomWidth: 2,
-  },
-  idText: {
-    color: '#ffffff',
-    backgroundColor: 'orange',
-    padding: 3,
-    borderRadius: 4,
-    marginBottom: 5,
-  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
     textDecorationLine: 'underline',
   },
-  textWhite: {
-    color: '#ffffff',
+  inputText: {
+    borderWidth: 1,
+    borderColor: 'lightblue',
+    borderRadius: 4,
+    padding: 10,
+    marginVertical: 15,
   },
-  underline: {
-    textDecorationLine: 'underline',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  viewContainer: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 8,
   },
 });
 
